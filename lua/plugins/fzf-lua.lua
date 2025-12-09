1d33c45b855bd1e9cd4349be7b1009e2c6510e65
Andrew Sukach <andrew@sukach.org>
2025-12-08 19:16:16 -0500

    fzf-lua: shortcut for diagnostics

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,9 +1,10 @@
 return {
-	"ibhagwan/fzf-lua",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
-	keys = {
-		{ '<leader>ff', '<cmd>FzfLua files<cr>',     mode = 'n' },
-		{ '<leader>fs', '<cmd>FzfLua live_grep<cr>', mode = 'n' }
-	}
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+  keys = {
+    { '<leader>ff', '<cmd>FzfLua files<cr>',                mode = 'n' },
+    { '<leader>fs', '<cmd>FzfLua live_grep<cr>',            mode = 'n' },
+    { '<leader>fd', '<cmd>FzfLua diagnostics_document<cr>', mode = 'n' }
+  }
 }
